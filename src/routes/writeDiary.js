b237b9f7a8f75c50dd31d2bd28aa0a9e196ce07c
gheun <gheun098@gmail.com>
2025-06-01 21:31:02 +0900

    thanks, regrets 라우터 병합

--- src/routes/writeDiary.js
+++ src/routes/writeDiary.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/writePostController');
+const controller = require('../controllers/writeDiaryController');
 
 router.post('/', controller.create);
 router.get('/', controller.getAll);
